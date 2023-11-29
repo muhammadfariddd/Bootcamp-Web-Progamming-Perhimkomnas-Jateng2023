9fb86867b36be48d4766cdeaf4602caab2bc287c
muhammadfariddd <muhammadfariddd2004@gmail.com>
2023-11-29 23:36:13 +0700

    second commit

---
+++ Muhammad Farid P-1/script.js
@@ -0,0 +1,7 @@
+ // Fungsi untuk mengubah teks pada elemen dengan ID "judul" dan "paragraf"
+ function ubahTeks() {
+    let nama = "Muhammad Farid";
+    document.getElementById("nama").innerHTML = nama;
+    document.getElementById("judul").innerHTML = "Universitas Islam Nahdlatul Ulama Jepara";
+    document.getElementById("paragraf").innerHTML = "Student and Progammer";
+}
